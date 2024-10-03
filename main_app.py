8d2b8ead2e3f4a8c3b86a50ddaccf0701bd4c67f
fff-12 <paunvprrv@gmail.com>
2024-10-03 17:59:56 +0300

    homework

--- main_app.py
+++ main_app.py
@@ -9,18 +9,63 @@ from kivy.core.window import Window
 from kivy.uix.scrollview import ScrollView
 from instructions import txt_instruction, txt_test1, txt_test2, txt_test3, txt_sits
 
-class CheckSist(Screen):
+class Result(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        outhe = BoxLayout(orientation = "vertical", padding = 8, spacing = 8)
+
+        lb1 = Label(text = "place for results")
+        
+        outhe.add_widget(lb1)
+
+        self.add_widget(outhe)
+
+class PulseScr2(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         outhe = BoxLayout(orientation = "vertical", padding = 8, spacing = 8)
         l1 = BoxLayout(size_hint = (0.8, None), height = "30sp")
+        l2 = BoxLayout(size_hint = (0.8, None), height = "30sp")
 
+        lb1 = Label(text = txt_test3)
+        lb2 = Label(text = "Результат:")
+        lb3 = Label(text = "Результата після відпочинку:")
+        self.btn = Button(text = "Завершити", size_hint = (0.3, 0.2), pos_hint = {"center_x": 0.5})
+        self.in_result1 = TextInput(text = "0", multiline = False)
+        self.in_result2 = TextInput(text = "0", multiline = False)
+        self.btn.on_press = self.next
+
+        l1.add_widget(lb2)
+        l1.add_widget(self.in_result1)
+        l2.add_widget(lb3)
+        l2.add_widget(self.in_result2)
+
+        outhe.add_widget(lb1)
+        outhe.add_widget(l1)
+        outhe.add_widget(l2)
+        outhe.add_widget(self.btn)
+
+        self.add_widget(outhe)
+
+    def next(self):
+        self.manager.current = "result"
+
+class CheckSist(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        outhe = BoxLayout(orientation = "vertical", padding = 8, spacing = 8)
+
+        lb1 = Label(text = txt_sits)
         self.btn = Button(text = "Продовжити", size_hint = (0.3, 0.2), pos_hint = {"center_x": 0.5})
+        self.btn.on_press = self.next
 
-        # self.btn.on_press = self.text
+        outhe.add_widget(lb1)
+        outhe.add_widget(self.btn)
 
-        # def next(self):
-        #     self.manager.current = "pulse2"
+        self.add_widget(outhe)
+
+    def next(self):
+        self.manager.current = "pulse2"
 
 class PulseScr(Screen):
     def __init__(self, **kwargs):
@@ -83,8 +128,8 @@ class HeartCheck(App):
         sm.add_widget(InstrScr(name = "instr"))
         sm.add_widget(PulseScr(name = "pulse1"))
         sm.add_widget(CheckSist(name = "sist"))
-        # sm.add_widget(PulseScr2(name = "pulse2"))
-        # sm.add_widget(Result(name = "result"))
+        sm.add_widget(PulseScr2(name = "pulse2"))
+        sm.add_widget(Result(name = "result"))
         return sm
 
 app = HeartCheck()
